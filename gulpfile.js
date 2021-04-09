5a6ce3105ad53dd0e37a2107f6b61cbfe5800b8a
Anton Ivanov <kupeirc@gmail.com>
2021-04-09 11:56:43 +0300

    fixed script module

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ exports.images = images;
 const scripts = () => {
   return gulp.src("source/js/*.js")
   .pipe(uglify())
-  .pipe(rename({suffix: ".min.js"}))
+  .pipe(rename({suffix: ".min"}))
   .pipe(gulp.dest("build/js"))
   .pipe(sync.stream())
 }
